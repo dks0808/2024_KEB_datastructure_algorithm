6851bfbfc027201352487e191743d782988a53b0
light <agy91521947@gmail.com>
2024-02-08 10:47:37 +0900

    v0.4 mymath import ver

--- day1.py
+++ day1.py
@@ -1,34 +1,7 @@
-# def factorial(n) -> int:
-#     result = 1
-#     for i in range(1,n+1):
-#         result = result * i
-#         return result
-
-def Combination(n,r) -> int:
-    """
-    this is combination func
-    :param n:
-    :param r:
-    :return:
-    """
-    numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(r)
-    return int(numerator / denominator)
-
-def factorial(num) -> int:
-    """
-    recusion
-    :param num:
-    :return:
-    """
-
-    if num == 1:
-        return 1
-    else:
-        return num * factorial(num-1)
+import mymath
 
 if __name__ == "__main__": # 메인인 함수를 알려주기 위함. 전역 변수를 정해주는 것.여러개의
     n = int(input("Enter entire number : "))
     r = int(input("Input you want combinations : "))
-    print(f"{n}C{r} = {Combination(n,r)}")
+    print(f"{n}C{r} = {mymath.Combination(n,r)}")
 
