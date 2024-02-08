52044890786c4bf1907aa6c55265c0fea6ad1ded
light <agy91521947@gmail.com>
2024-02-08 10:41:36 +0900

    v 0.2 combination by recursion preview

--- day1.py
+++ day1.py
@@ -1,8 +1,8 @@
-def factorial(n) -> int:
-    for i in range(0,n):
-        i
-
-    pass
+# def factorial(n) -> int:
+#     result = 1
+#     for i in range(1,n+1):
+#         result = result * i
+#         return result
 
 def Combination(n,r) -> int:
     """
@@ -13,10 +13,22 @@ def Combination(n,r) -> int:
     """
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
-    return numerator/ denominator
+    return int(numerator / denominator)
 
-if __name__ = "__main__": # 메인인 함수를 알려주기 위함. 전역 변수를 정해주는 것.여러개의
-    n = int(input("Enter entire number n : "))
-    r = int(input("Input you want combi : "))
-    print(f"{n}C{r} = {nCr(n,r)}")
+def factorial(num) -> int:
+    """
+    recusion
+    :param num:
+    :return:
+    """
+
+    if num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+if __name__ == "__main__": # 메인인 함수를 알려주기 위함. 전역 변수를 정해주는 것.여러개의
+    n = int(input("Enter entire number : "))
+    r = int(input("Input you want combinations : "))
+    print(f"{n}C{r} = {Combination(n,r)}")
 
