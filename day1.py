e4966b6878c28a12c6d8190ae254fdc4b2c0552e
light <agy91521947@gmail.com>
2024-02-08 10:51:50 +0900

    v0.5 some message

--- day1.py
+++ day1.py
@@ -1,4 +1,7 @@
 import mymath
+# recursion func is more slow than repeat
+# because repeat is use register or cash memory recursion func is use main memory
+
 
 if __name__ == "__main__": # 메인인 함수를 알려주기 위함. 전역 변수를 정해주는 것.여러개의
     n = int(input("Enter entire number : "))
