ed85815a726e7fffe84e6b7860236637340818a2
light <agy91521947@gmail.com>
2024-02-15 10:59:49 +0900

    fibonacci gui on processing...

--- day03.py
+++ day03.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 def fibo_recursion(number: int) -> int:
     """
     fibonacci function by recursion.
@@ -25,8 +26,8 @@ def fibo_repetition(number: int) -> int:
     return a
 
 
-# memo = [0 if i == 0 else 1 if i == 1 else None for i in range(100)]
-memo = [0, 1] + [None] * (100-1)
+memo = [0 if i == 0 else 1 if i == 1 else None for i in range(100)]
+# memo = [0, 1] + [None] * (100-1)
 
 
 
@@ -47,12 +48,28 @@ def fibo_memoization(number: int) -> int:
         result = fibo_memoization(number-1) + fibo_memoization(number-2)
         memo[number] = result
     return result
+w = tk.Tk()
+w.title("Fibonacci")
+w.geometry("250x150")
+lbl_display_fibonacci_result = tk.Lable(w, text = "Fibonacci by memoization")
+en_input_number = tk.Entry(w)
+btn_click = tk.button(w, text = "Click")
+
+lbl_display_fibonacci_result.pack()
+en_input_number.pack()
+btn_click.pack(fill = "x")
+
+w.mainloop()
 
 
 
-n = int(input("Input number : ")) #input box
 
-print(f"fibonacci{n} = {fibo_memoization(n)}") #lable
+# n = int(input("Input number : ")) #input box
+#
+# print(f"fibonacci{n} = {fibo_memoization(n)}") #lable
+#
+#
+
 # for i in range(0, n):
 #     print(i)
 #     print(fibo_memoization(i))
