21d3285bf498d62f65099b3bf67871fc82e87ffa
light <agy91521947@gmail.com>
2024-02-16 14:11:50 +0900

    baekjoon silver1

--- day03.py
+++ day03.py
@@ -1,17 +1,35 @@
-## 함수 선언 부분 ##
-import random
-def insertionSort(ary) :
-	n = len(ary)
-	for end in range(1, n) :
-		for cur in range(end, 0, -1) :
-			if ( ary[cur-1] > ary[cur] ) :
-				ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
-	return ary
+# baekjoonk
+from collections import deque
+input_data = list(map(int, input().split()))
 
-## 전역 변수 선언 부분 ##
-dataAry = [random.randint(0,100) for _ in range(10)]
+print(list(input_data))
+n = input_data[0]
+m = input_data[1]
+k = input_data[2]
+x = input_data[3]
 
-## 메인 코드 부분 ##
-print('정렬 전 -->', dataAry)
-dataAry = insertionSort(dataAry)
-print('정렬 후 -->', dataAry)
+print(n, m, k, x)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+
+distance = [-1]*(n+1)
+distance[x] = 0
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next in graph[now]:
+        if distance[next] ==-1:
+            distance[next] = distance[now] +1
+            q.append(next)
+
+    check = False
+    for  i in range(1, n +1):
+        if distance[i] == k:
+            print[i]
+            check = True
+    if check == False:
+        print(-1)
+
